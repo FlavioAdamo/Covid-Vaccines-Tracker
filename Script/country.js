e2bde607ca46b6e84c2e1080e91279ed2b06d376
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2021-05-22 15:19:36 +0300

    Added contribute button, didn't seem to work before

--- Script/country.js
+++ Script/country.js
@@ -2,7 +2,7 @@ var countryName = "";
 var countryData = [];
 var countryLastData = [];
 
-$( document ).ready(async () => {  
+$(document).ready(async () => {
     await loadVariables();
     countryName = await GetCountryName();
     countryData = await GetCountryData(countryName);
@@ -12,53 +12,53 @@ $( document ).ready(async () => {
     loadCounterScript();
 });
 
-async function GetCountryName(){
+async function GetCountryName() {
     // Get the name of the country by reading di url
     return window.location.search.substring(1).split("&");
 }
 
-async function GetCountryLastData(){
+async function GetCountryLastData() {
     // Get the latest data avaible for each country
     return countryData[countryData.length - 1].split(',');
 }
 
-async function loadCountryCountersTemplate(data){
+async function loadCountryCountersTemplate(data) {
     const TEMPLATE = document.querySelector("#country_counters_template");
     const CONTENT = TEMPLATE.content.cloneNode(true);
     // Load the counters on top
     $("#country_counters").append(
-        COUNTRY_COUNTERS_TEMPLATE .replace('{{totvalue}}', data[3])
-        .replace('{{totvaccinated}}',data[9] + "%")
-        .replace('{{fullyVaccinated}}' , data[10] + "%")
-        .replace('{{peopleVaccinated}}' , data[4])
+        COUNTRY_COUNTERS_TEMPLATE.replace('{{totvalue}}', data[3])
+            .replace('{{totvaccinated}}', data[9] + "%")
+            .replace('{{fullyVaccinated}}', data[10] + "%")
+            .replace('{{peopleVaccinated}}', data[4])
     );
 
 }
 
-function hideCountersWithNoData(data){
+function hideCountersWithNoData(data) {
     // Hide the counters on the top if the the data is not avaible
-    if(data[3] == null || data[3] == "") {
+    if (data[3] == null || data[3] == "") {
         $("#total_doses").hide();
     }
-    if(data[4] == null || data[4] == "") {
+    if (data[4] == null || data[4] == "") {
         $("#people_vaccinated").hide();
     }
-    if(data[9] == null || data[9] == "") {
+    if (data[9] == null || data[9] == "") {
         $("#vaccinated_percent").hide();
     }
-    if(data[10] == null || data[10] == "") {
+    if (data[10] == null || data[10] == "") {
         $("#fully_vaccinated_percent").hide();
     }
 }
 
-function loadCounterScript(){
+function loadCounterScript() {
     // Load the animation to the counter
     // actually is a copy & paste from some russian website 
-    $('.counter').each(function() {
-        $(this).prop('Counter', 0.0).animate({Counter: $(this).text()}, {
+    $('.counter').each(function () {
+        $(this).prop('Counter', 0.0).animate({ Counter: $(this).text() }, {
             duration: 2000,
             easing: 'swing',
-            step: function(now) {
+            step: function (now) {
                 $(this).text(formatNumberWithCommas(Math.ceil(now)));
             }
         });
